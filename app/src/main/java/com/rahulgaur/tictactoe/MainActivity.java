df5fc34620b60deedf9cc898c52c5691030b512f
Rahul Gaur <rahul.gaur152@gmail.com>
2018-09-13 08:17:53 +0530

    made Welcome page and a rough idea of mainActivity, welcome page = it saved user name in sharedpreferences and use that data to show the name and greet the player, mainActivity = right now it keeps track of turn of player 1 and player 2. working on keeping track of when the game is over(DRAW)
    TODO: function for win condition

--- app/src/main/java/com/rahulgaur/tictactoe/MainActivity.java
+++ app/src/main/java/com/rahulgaur/tictactoe/MainActivity.java
@@ -2,12 +2,141 @@ package com.rahulgaur.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button B1, B2, B3, B4, B5, B6, B7, B8, B9;
+    private int turn = 0;
+    private int gameover = 1;
+    private TextView player1, player2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        B1 = findViewById(R.id.b1);
+        B2 = findViewById(R.id.b2);
+        B3 = findViewById(R.id.b3);
+        B4 = findViewById(R.id.b4);
+        B5 = findViewById(R.id.b5);
+        B6 = findViewById(R.id.b6);
+        B7 = findViewById(R.id.b7);
+        B8 = findViewById(R.id.b8);
+        B9 = findViewById(R.id.b9);
+
+        player1 = findViewById(R.id.player1TV);
+        player2 = findViewById(R.id.player2TV);
+
+        Toast.makeText(this, "Player 1 Turn", Toast.LENGTH_LONG).show();
+
+        B1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                B1.setEnabled(false);
+                playerTurn(turn);
+            }
+        });
+
+        B2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                B2.setEnabled(false);
+                playerTurn(turn);
+
+            }
+        });
+        B3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                B3.setEnabled(false);
+                playerTurn(turn);
+
+            }
+        });
+
+        B4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                B4.setEnabled(false);
+                playerTurn(turn);
+
+            }
+        });
+
+        B5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                B5.setEnabled(false);
+                playerTurn(turn);
+
+            }
+        });
+
+        B6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                B6.setEnabled(false);
+                playerTurn(turn);
+
+            }
+        });
+
+        B7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                B7.setEnabled(false);
+                playerTurn(turn);
+
+            }
+        });
+
+        B8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                B8.setEnabled(false);
+                playerTurn(turn);
+
+            }
+        });
+
+        B9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                B9.setEnabled(false);
+                playerTurn(turn);
+
+            }
+        });
+
+    }
+
+    private void playerTurn(int pt) {
+        if (pt == 0){
+            String sourceString2 = "<b>Player 2</b>";
+            player2.setText(Html.fromHtml(sourceString2));
+            String sourceString1 = "Player 1";
+            player1.setText(Html.fromHtml(sourceString1));
+            Toast.makeText(this, "Player 2 turn", Toast.LENGTH_SHORT).show();
+            turn++;
+            gameover++;
+        } else {
+            turn--;
+            String sourceString1 = "<b>Player 1</b>";
+            player1.setText(Html.fromHtml(sourceString1));
+            String sourceString2 = "Player 2";
+            player2.setText(Html.fromHtml(sourceString2));
+            Toast.makeText(this, "Player 1 turn", Toast.LENGTH_SHORT).show();
+            gameover++;
+        }
+
+        if (gameover==9){
+            Toast.makeText(this, "GAME OVER :D", Toast.LENGTH_SHORT).show();
+        }
     }
 }
---
+++ app/src/main/java/com/rahulgaur/tictactoe/Welcome.java
@@ -0,0 +1,99 @@
+package com.rahulgaur.tictactoe;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Objects;
+
+public class Welcome extends AppCompatActivity {
+    private String username;
+    private TextView nameTV;
+    private EditText nameET;
+    private Button submitBtn;
+    private String TAG = "Welcome.java";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        submitBtn = findViewById(R.id.welcome_submitBtn);
+        Button playBtn = findViewById(R.id.welcome_playBtn);
+
+        nameET = findViewById(R.id.welcome_editText);
+        nameTV = findViewById(R.id.welcome_userTV);
+
+        pref();
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                username = nameET.getText().toString();
+
+                if (!username.isEmpty()) {
+                    SharedPreferences.Editor editor = getSharedPreferences("USERNAME", MODE_PRIVATE).edit();
+                    editor.putString("name", username);
+                    editor.apply();
+                    nameTV.setText("Welcome\n" + username);
+                    nameET.setVisibility(View.INVISIBLE);
+                    nameET.setEnabled(false);
+                    submitBtn.setEnabled(false);
+                    submitBtn.setVisibility(View.INVISIBLE);
+                } else {
+                    Toast.makeText(Welcome.this, "Please Enter Your Name! :)", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        nameTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nameET.setVisibility(View.VISIBLE);
+                nameET.setEnabled(true);
+                submitBtn.setEnabled(true);
+                submitBtn.setVisibility(View.VISIBLE);
+            }
+        });
+
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Welcome.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    @SuppressLint("SetTextI18n")
+    void pref() {
+        final SharedPreferences prefs = getSharedPreferences("USERNAME", MODE_PRIVATE);
+        try {
+            String name = prefs.getString("name", null); //"No name defined" is the default value
+            if (!Objects.requireNonNull(name).isEmpty()) {
+                Toast.makeText(this, "Welcome Back " + name, Toast.LENGTH_SHORT).show();
+                nameTV.setText("Welcome\n" + name);
+                nameET.setVisibility(View.INVISIBLE);
+                nameET.setEnabled(false);
+                submitBtn.setEnabled(false);
+                submitBtn.setVisibility(View.INVISIBLE);
+            } else {
+                Log.e(TAG, "pref: No prefs found");
+                nameET.setVisibility(View.VISIBLE);
+                nameET.setEnabled(true);
+                submitBtn.setEnabled(true);
+                submitBtn.setVisibility(View.VISIBLE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
